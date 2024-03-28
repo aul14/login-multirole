c57c30b2c0de39d25a04a485b8364245c5783c0a
aul14 <rahmanaulia336@gmail.com>
2024-03-28 14:35:40 +0700

    protected dashboard

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
 import logo from '../logo.png';
+import { useDispatch, useSelector } from 'react-redux'
+import { NavLink, useNavigate } from 'react-router-dom'
+import { LogOut, reset } from '../features/authSlice'
 
 const Navbar = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const { user } = useSelector((state) => state.auth);
+
+    const logout = () => {
+        dispatch(LogOut());
+        dispatch(reset());
+        navigate("/");
+    }
     return (
         <div>
             <nav className="navbar is-fixed-top has-shadow" role="navigation" aria-label="main navigation">
@@ -23,7 +34,7 @@ const Navbar = () => {
                     <div className="navbar-end">
                         <div className="navbar-item">
                             <div className="buttons">
-                                <button className="button is-light">
+                                <button onClick={logout} className="button is-light">
                                     Logout
                                 </button>
                             </div>
