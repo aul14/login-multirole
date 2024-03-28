a3e4049b69cd2d5e954eee6b767cf1c9055afa69
aul14 <rahmanaulia336@gmail.com>
2024-03-28 16:19:42 +0700

    form add user

--- frontend/src/components/FormAddUser.jsx
+++ frontend/src/components/FormAddUser.jsx
@@ -1,6 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const FormAddUser = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confPassword, setConfPassword] = useState("");
+    const [role, setRole] = useState("");
+    const [msg, setMsg] = useState("");
+
+    const navigate = useNavigate();
+
+    const saveUser = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.post('http://localhost:5000/users', {
+                name: name,
+                email: email,
+                password: password,
+                confPassword: confPassword,
+                role: role
+            })
+            navigate('/users');
+        } catch (error) {
+            if (error.response) {
+                setMsg(error.response.data.msg)
+            }
+        }
+    }
     return (
         <div>
             <h1 className='title'>Users</h1>
@@ -8,36 +36,37 @@ const FormAddUser = () => {
             <div className="card is-shadowless">
                 <div className="card-content">
                     <div className="content">
-                        <form>
+                        <form onSubmit={saveUser}>
+                            <p className="has-text-centered">{msg}</p>
                             <div className="field">
                                 <label className="label">Name</label>
                                 <div className="control">
-                                    <input type="text" placeholder='Name' className="input" />
+                                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder='Name' className="input" />
                                 </div>
                             </div>
                             <div className="field">
                                 <label className="label">Email</label>
                                 <div className="control">
-                                    <input type="text" placeholder='Email' className="input" />
+                                    <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Email' className="input" />
                                 </div>
                             </div>
                             <div className="field">
                                 <label className="label">Password</label>
                                 <div className="control">
-                                    <input type="password" placeholder='******' className="input" />
+                                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='******' className="input" />
                                 </div>
                             </div>
                             <div className="field">
                                 <label className="label">Confirm Password</label>
                                 <div className="control">
-                                    <input type="password" placeholder='******' className="input" />
+                                    <input type="password" value={confPassword} onChange={(e) => setConfPassword(e.target.value)} placeholder='******' className="input" />
                                 </div>
                             </div>
                             <div className="field">
                                 <label className="label">Role</label>
                                 <div className="control">
                                     <div className="select is-fullwidth">
-                                        <select>
+                                        <select value={role} onChange={(e) => setRole(e.target.value)}>
                                             <option value="admin">Admin</option>
                                             <option value="user">User</option>
                                         </select>
@@ -46,7 +75,7 @@ const FormAddUser = () => {
                             </div>
                             <div className="field">
                                 <div className="control">
-                                    <button className="button is-primary">Save</button>
+                                    <button type='submit' className="button is-primary">Save</button>
                                 </div>
                             </div>
                         </form>
